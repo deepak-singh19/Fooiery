9e5b8efef1e71578a3eb006a4d96f0cde513dd73
deepak-singh19 <48211820+deepak-singh19@users.noreply.github.com>
2023-01-16 19:59:09 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtk8zDi09vDMgj-7NmGVdDO5URoniJ24I",
-    authDomain: "fooliery.firebaseapp.com",
-    databaseURL: "https://fooliery-default-rtdb.firebaseio.com",
-    projectId: "fooliery",
-    storageBucket: "fooliery.appspot.com",
-    messagingSenderId: "686092038183",
-    appId: "1:686092038183:web:e0fa3379ea8c51caea5e02"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = getApp.Length > 0 ? getApp() : initializeApp(firebaseConfig);
