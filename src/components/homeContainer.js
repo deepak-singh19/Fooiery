51f6491964b728ae76c33454a3628109513b26f6
deepak-singh19 <48211820+deepak-singh19@users.noreply.github.com>
2023-03-01 03:59:37 +0530

    Updated Home container

--- src/components/homeContainer.js
+++ src/components/homeContainer.js
@@ -35,13 +35,13 @@ const HomeContainer = () => {
         
             <img src={HeroBg} className="ml-auto h-420 w-full lg:w-auto lg:h-650" alt="hero-bg"/>
 
-            <div className="w-full h-full absolute  top-0 left-0 flex items-center justify-center  md:px-32 gap-4 flex-wrap py-4">
+            <div className="w-full h-full absolute  top-0 left-0 flex items-center justify-center  md:px-20 gap-4 flex-wrap py-4">
 
                 {HeroData && HeroData.map(n =>(
 
                     
-                    <div key={n.id} className='  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl  flex flex-col items-center justify-center'>
-                    <img src={n.imageSrc} className=" w-20 lg:w-40 -mt-10lg:-mt-20 "  alt="I1"/>
+                    <div key={n.id} className='  md:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl  flex flex-col items-center justify-center'>
+                    <img src={n.imageSrc} className=" w-20 md:w-40 -mt-10 md:-mt-20 "  alt="I1"/>
                     <p className='text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4'>{n.name}</p>
                     <p className='text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3'>{n.decp}</p>
                     <p className='text-sm font-semibold text-headingColor'><span className='text-xs text-red-600'>Rs.</span>{n.price}</p>
